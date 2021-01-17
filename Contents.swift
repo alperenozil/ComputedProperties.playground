63cd46d1875bce31a5dd53fb53b657855625bd92
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-01-17 21:52:17 +0300

    Update Contents.swift

--- Contents.swift
+++ Contents.swift
@@ -1,10 +1,10 @@
 import UIKit
 
 let defaults=UserDefaults.standard
-/*
+
 defaults.setValue("alperen", forKey: "name")
 defaults.setValue("ozil", forKey: "surname")
 defaults.setValue("25", forKey: "age")
- */
-let name=defaults.string(forKey: "name")
+
+let name=defaults.value(forKey: "name")
 print(String(describing: name))
